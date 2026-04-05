75572060ec87e164fcbc44cac7b25d60d28ab77f
Reshufowzi <114790414+Reshufowzi@users.noreply.github.com>
2026-04-05 11:34:07 +0530

    Update UserRepository.java

--- user-service/src/main/java/com/example/user/repo/UserRepository.java
+++ user-service/src/main/java/com/example/user/repo/UserRepository.java
@@ -1,3 +1,9 @@
-public interface UserRepository extends JpaRepository<User, Integer> {
+package com.example.user.repo;
+
+import com.example.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
     User findByEmail(String email);
 }
