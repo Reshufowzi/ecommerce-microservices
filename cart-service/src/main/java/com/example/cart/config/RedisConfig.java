53642258280f2c64062bc147cc0b0bc88517cb11
Reshufowzi <114790414+Reshufowzi@users.noreply.github.com>
2026-04-05 12:32:11 +0530

    Update RedisConfig.java

--- cart-service/src/main/java/com/example/cart/config/RedisConfig.java
+++ cart-service/src/main/java/com/example/cart/config/RedisConfig.java
@@ -1,10 +1,17 @@
+package com.example.cart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
 @Configuration
 public class RedisConfig {
 
     @Bean
-    public RedisTemplate<String, Object> template(RedisConnectionFactory factory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
+        template.setConnectionFactory(connectionFactory);
         return template;
     }
 }
