4addf281b98211944b766aa07b047c88f4671855
Reshufowzi <114790414+Reshufowzi@users.noreply.github.com>
2026-04-05 12:47:13 +0530

    Update PaymentRepository.java

--- payment-service/src/main/java/com/example/payment/repo/PaymentRepository.java
+++ payment-service/src/main/java/com/example/payment/repo/PaymentRepository.java
@@ -1,2 +1,7 @@
+package com.example.payment.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.payment.entity.Payment;
+
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 }
