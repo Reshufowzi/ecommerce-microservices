b3ef008a26a3946aa8041f2d83dad056239c16d2
Reshufowzi <114790414+Reshufowzi@users.noreply.github.com>
2026-04-05 12:24:11 +0530

    Update ProductRepository.java

--- product-service/src/main/java/com/example/product/repo/ProductRepository.java
+++ product-service/src/main/java/com/example/product/repo/ProductRepository.java
@@ -1,2 +1,7 @@
+package com.example.product.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.product.entity.Product;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
