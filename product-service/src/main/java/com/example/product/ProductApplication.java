1fb1186c1651586df4fcc98dfe0285918823e28f
Reshufowzi <114790414+Reshufowzi@users.noreply.github.com>
2026-04-05 12:25:11 +0530

    Update ProductApplication.java

--- product-service/src/main/java/com/example/product/ProductApplication.java
+++ product-service/src/main/java/com/example/product/ProductApplication.java
@@ -1,3 +1,8 @@
+package com.example.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ProductApplication {
     public static void main(String[] args) {
